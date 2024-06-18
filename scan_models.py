55e5fb7c029574c9864eee287bce95646567f08e
James623 <52039577+James623@users.noreply.github.com>
2024-06-18 13:37:07 +0700

    Create scan_models.py

---
+++ scan_models.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+from art.estimators.classification import TensorFlowV2Classifier
+from art.attacks.evasion import FastGradientMethod
+from art.utils import load_mnist
+
+# Загрузите модель
+model = tf.keras.models.load_model('mlmodels/model.h5')  # Путь к вашей модели
+
+# Создайте классификатор ART
+classifier = TensorFlowV2Classifier(
+    model=model,
+    nb_classes=10,
+    input_shape=(28, 28, 1),
+    loss_object=tf.keras.losses.CategoricalCrossentropy()
+)
+
+# Загрузите данные для тестирования
+(x_train, y_train), (x_test, y_test), min_, max_ = load_mnist()
+
+# Выполните атаку
+attack = FastGradientMethod(estimator=classifier, eps=0.2)
+x_test_adv = attack.generate(x=x_test)
+
+# Оцените модель на уязвимости
+predictions = classifier.predict(x_test_adv)
+accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / y_test.shape[0]
+print(f"Accuracy on adversarial test examples: {accuracy * 100}%")
