1b0f092ff590b72858e52634221f21a0e5d9a0c6
James623 <52039577+James623@users.noreply.github.com>
2024-06-19 09:16:07 +0700

    Update scan_models.py

--- scan_models.py
+++ scan_models.py
@@ -8,11 +8,18 @@ import os
 # Выведем текущую рабочую директорию
 print("Current working directory:", os.getcwd())
 
-# Загрузите модель
+# Путь к модели
 model_path = 'mlmodels/model.h5'
-model = tf.keras.models.load_model(model_path)
 
-# Создайте классификатор ART
+# Попытаемся загрузить модель
+try:
+    model = tf.keras.models.load_model(model_path)
+    print("Модель успешно загружена.")
+except OSError:
+    print(f"Не удалось загрузить модель по пути: {model_path}")
+    exit(1)  # Выход из скрипта в случае ошибки загрузки модели
+
+# Создаем классификатор ART
 classifier = TensorFlowV2Classifier(
     model=model,
     nb_classes=10,
@@ -20,14 +27,14 @@ classifier = TensorFlowV2Classifier(
     loss_object=tf.keras.losses.CategoricalCrossentropy()
 )
 
-# Загрузите данные для тестирования
+# Загружаем данные для тестирования
 (x_train, y_train), (x_test, y_test), min_, max_ = load_mnist()
 
-# Выполните атаку
+# Выполняем атаку
 attack = FastGradientMethod(estimator=classifier, eps=0.2)
 x_test_adv = attack.generate(x=x_test)
 
-# Оцените модель на уязвимости
+# Оцениваем модель на уязвимости
 predictions = classifier.predict(x_test_adv)
 accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / y_test.shape[0]
-print(f"Accuracy on adversarial test examples: {accuracy * 100}%")
+print(f"Точность на адверсариальных тестовых примерах: {accuracy * 100}%")
