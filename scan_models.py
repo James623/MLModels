942b820908cf96fb3107e54e47f7597f1f8a8dc5
James623 <52039577+James623@users.noreply.github.com>
2024-06-20 09:53:11 +0700

    Update scan_models.py

--- scan_models.py
+++ scan_models.py
@@ -2,39 +2,32 @@ import numpy as np
 import tensorflow as tf
 from art.estimators.classification import TensorFlowV2Classifier
 from art.attacks.evasion import FastGradientMethod
-from art.utils import load_mnist
+from art.utils import load_cifar10
 import os
 
 # Выведем текущую рабочую директорию
 print("Current working directory:", os.getcwd())
 
-# Путь к модели
-model_path = 'mlmodels/model.h5'
+# Загрузите модель
+model_path = 'mlmodels/cifar10_model.h5'
+model = tf.keras.models.load_model(model_path)
 
-# Попытаемся загрузить модель
-try:
-    model = tf.keras.models.load_model(model_path)
-    print("Модель успешно загружена.")
-except OSError:
-    print(f"Не удалось загрузить модель по пути: {model_path}")
-    exit(1)  # Выход из скрипта в случае ошибки загрузки модели
-
-# Создаем классификатор ART
+# Создайте классификатор ART
 classifier = TensorFlowV2Classifier(
     model=model,
     nb_classes=10,
-    input_shape=(28, 28, 1),
+    input_shape=(32, 32, 3),
     loss_object=tf.keras.losses.CategoricalCrossentropy()
 )
 
-# Загружаем данные для тестирования
-(x_train, y_train), (x_test, y_test), min_, max_ = load_mnist()
+# Загрузите данные для тестирования
+(x_train, y_train), (x_test, y_test), min_, max_ = load_cifar10()
 
-# Выполняем атаку
+# Выполните атаку
 attack = FastGradientMethod(estimator=classifier, eps=0.2)
 x_test_adv = attack.generate(x=x_test)
 
-# Оцениваем модель на уязвимости
+# Оцените модель на уязвимости
 predictions = classifier.predict(x_test_adv)
 accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / y_test.shape[0]
-print(f"Точность на адверсариальных тестовых примерах: {accuracy * 100}%")
+print(f"Accuracy on adversarial test examples: {accuracy * 100}%")
