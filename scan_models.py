60ad8e2c91408e07d481210df2c2442616457d69
James623 <52039577+James623@users.noreply.github.com>
2024-06-25 09:16:57 +0700

    Update scan_models.py

--- scan_models.py
+++ scan_models.py
@@ -1,36 +1,26 @@
-import numpy as np
 import tensorflow as tf
-from art.estimators.classification import TensorFlowV2Classifier
+from art.estimators.classification import KerasClassifier
 from art.attacks.evasion import FastGradientMethod
-from art.utils import load_cifar10
-import os
+from art.utils import load_mnist
 
-# Выведем текущую рабочую директорию
-print("Current working directory:", os.getcwd())
+# Путь к модели
+model_path = 'mlmodels/mnist_model.h5'
 
-# Загрузите модель
-model_path = '/resnet50_imagenet.h5'
+# Загрузка модели
 model = tf.keras.models.load_model(model_path)
 
-# Создайте классификатор ART
-classifier = TensorFlowV2Classifier(
-    model=model,
-    nb_classes=1000,  # ImageNet имеет 1000 классов
-    input_shape=(224, 224, 3),
-    loss_object=tf.keras.losses.CategoricalCrossentropy()
-)
+# Создание KerasClassifier
+classifier = KerasClassifier(model=model)
 
-# Загрузите данные для тестирования (измените на подходящий набор данных для вашего случая)
-(x_train, y_train), (x_test, y_test), min_, max_ = load_cifar10()
+# Загрузка данных MNIST
+(x_train, y_train), (x_test, y_test), _, _ = load_mnist()
 
-# Измените размер тестовых данных до размера входных данных модели ResNet-50
-x_test = tf.image.resize(x_test, (224, 224)).numpy()
-
-# Выполните атаку
+# Создание атакующего метода
 attack = FastGradientMethod(estimator=classifier, eps=0.2)
+
+# Создание атакующих примеров
 x_test_adv = attack.generate(x=x_test)
 
-# Оцените модель на уязвимости
-predictions = classifier.predict(x_test_adv)
-accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / y_test.shape[0]
-print(f"Accuracy on adversarial test examples: {accuracy * 100}%")
+# Оценка модели на атакующих примерах
+accuracy = classifier._model.evaluate(x_test_adv, y_test)[1]
+print(f'Accuracy on adversarial test examples: {accuracy * 100:.2f}%')
